02f51361a2eefab8a68ba0305ab533e861b0ec2c
Paul FIORENTINO <fiorentino.paulalain@gmail.com>
2015-06-17 22:19:09 +0200

    fix splashscreen settings and database first import

--- app/src/main/java/fr/epsi/i4/kitchenguesser/SplashScreen.java
+++ app/src/main/java/fr/epsi/i4/kitchenguesser/SplashScreen.java
@@ -1,15 +1,22 @@
 package fr.epsi.i4.kitchenguesser;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
 
-public class SplashScreen extends ActionBarActivity {
+
+public class SplashScreen extends Activity {
 
     private RelativeLayout accueil = null;
 
@@ -17,42 +24,48 @@ public class SplashScreen extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        accueil = (RelativeLayout) this.findViewById(R.id.splashScreen);
 
+        accueil = (RelativeLayout) this.findViewById(R.id.splashScreen);
         accueil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                //intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                finish();
                 startActivity(intent);
-                // overridePendingTransition (0, 0);
-
             }
         });
 
+        initializeDB();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
-        return true;
-    }
+    private void initializeDB() {
+        String appDataPath = Environment.getDataDirectory()+File.separator+"data"+File.separator+getPackageName();
+        File dbExistsFile = new File(appDataPath+File.separator+"database_imported");
+        if (!dbExistsFile.exists()){
+            Log.d("import", "On importe la base !");
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            // Pour créer la base si elle n'existe pas encore
+            KitchenGuesserOpenHelper mDbHelper = new KitchenGuesserOpenHelper(this.getApplicationContext());
+            mDbHelper.getReadableDatabase();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            String dbPath = appDataPath+File.separator+"databases"+File.separator+"KitchenGuesser.db";
+            File appDB = new File(dbPath);
+
+            try {
+                InputStream src = this.getResources().openRawResource(R.raw.kitchenguesser);
+                ReadableByteChannel rbc = Channels.newChannel(src);
+                FileChannel dst = new FileOutputStream(appDB).getChannel();
+
+                Utils.fastChannelCopy(rbc, dst);
+
+                src.close();
+                dst.close();
+
+                dbExistsFile.createNewFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.d("import", "On n'importe PAS la base !");
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
