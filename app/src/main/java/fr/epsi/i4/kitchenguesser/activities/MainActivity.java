47f068832dbdf4e8fa311f3787746ab0cda85fc5
Paul FIORENTINO <fiorentino.paulalain@gmail.com>
2015-06-21 00:54:13 +0200

    misc fixes

--- app/src/main/java/fr/epsi/i4/kitchenguesser/activities/MainActivity.java
+++ app/src/main/java/fr/epsi/i4/kitchenguesser/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package fr.epsi.i4.kitchenguesser.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -104,7 +107,29 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public void onBackPressed() {
         if (!rollBack()){
-            super.onBackPressed();
+            AlertDialog dlg = null;
+
+            if (!this.isFinishing()) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                builder.setTitle(R.string.dialog_exit_game_title)
+                        .setMessage(R.string.dialog_exit_game)
+                        .setCancelable(false)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                MainActivity.super.onBackPressed();
+                                dialog.dismiss();
+                            }
+                        })
+                        .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                dlg = builder.create();
+                dlg.show();
+            }
         }
     }
 
--- app/src/main/java/fr/epsi/i4/kitchenguesser/activities/PlayAgainActivity.java
+++ app/src/main/java/fr/epsi/i4/kitchenguesser/activities/PlayAgainActivity.java
@@ -1,5 +1,7 @@
 package fr.epsi.i4.kitchenguesser.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -44,6 +46,33 @@ public class PlayAgainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog dlg = null;
+
+        if (!this.isFinishing()) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setTitle(R.string.dialog_exit_game_title)
+                    .setMessage(R.string.dialog_exit_game)
+                    .setCancelable(false)
+                    .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                            noClick(null);
+                        }
+                    })
+                    .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                        }
+                    });
+
+            dlg = builder.create();
+            dlg.show();
+        }
     }
 
     public void yesClick(View v) {
